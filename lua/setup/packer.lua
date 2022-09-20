c6e418fbc82ab0c1ba98afc1278c296485328ea8
philbev <phil@philbev.co.uk>
2022-09-20 12:25:04 +0100

    packerlua: Bootstrap enabled.

--- lua/setup/packer.lua
+++ lua/setup/packer.lua
@@ -6,6 +6,20 @@
 
 -- ~/.config/nvim/lua/setup/packer.lua
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+
 local status_ok,packer = pcall(require, 'packer')
 if not status_ok then
     return
@@ -20,36 +34,40 @@ packer.init{
 }
 
 return packer.startup(function()
-    use 'wbthomason/packer.nvim'
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use 'kyazdani42/nvim-web-devicons'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'onsails/lspkind.nvim'
-    use 'uga-rosa/cmp-dictionary'
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'norcalli/nvim-colorizer.lua'
-    use 'windwp/nvim-autopairs'
-    use 'p00f/nvim-ts-rainbow'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-lua/plenary.nvim'
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use 'nvim-lualine/lualine.nvim'
-    use 'Mofiqul/dracula.nvim'
-    use 'lifepillar/vim-gruvbox8'
-    use 'navarasu/onedark.nvim'
-    use { 'nvim-neo-tree/neo-tree.nvim', branch = 'v2.x' }
-    use 'MunifTanjim/nui.nvim'
-    use 'folke/which-key.nvim'
-    use 'akinsho/toggleterm.nvim'
-    use 'tpope/vim-surround'
+	use 'wbthomason/packer.nvim'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'kyazdani42/nvim-web-devicons'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'L3MON4D3/LuaSnip'
+	use 'saadparwaiz1/cmp_luasnip'
+	use 'onsails/lspkind.nvim'
+	use 'uga-rosa/cmp-dictionary'
+	use 'hrsh7th/cmp-nvim-lua'
+	use 'norcalli/nvim-colorizer.lua'
+	use 'windwp/nvim-autopairs'
+	use 'p00f/nvim-ts-rainbow'
+	use 'lukas-reineke/indent-blankline.nvim'
+	use 'nvim-telescope/telescope.nvim'
+	use 'nvim-lua/plenary.nvim'
+	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+	use 'nvim-lualine/lualine.nvim'
+	use 'Mofiqul/dracula.nvim'
+	use 'lifepillar/vim-gruvbox8'
+	use 'navarasu/onedark.nvim'
+	use { 'nvim-neo-tree/neo-tree.nvim', branch = 'v2.x' }
+	use 'MunifTanjim/nui.nvim'
+	use 'folke/which-key.nvim'
+	use 'akinsho/toggleterm.nvim'
+	use 'tpope/vim-surround'
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
 
