de59594ffba7f1f88931905562105198278c872e
philbev <phil@philbev.co.uk>
2022-09-21 10:17:15 +0100

    completion.lua: Protected calls.

--- lua/setup/completion.lua
+++ lua/setup/completion.lua
@@ -11,14 +11,22 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
--- local luasnip = require("luasnip")
 local status_ok,luasnip = pcall(require, 'luasnip')
 if not status_ok then
     return
 end
 
-local lspkind = require('lspkind')
-local cmp = require'cmp'
+local status_ok,lspkind = pcall(require, 'lspkind')
+if not status_ok then
+    return
+end
+
+local lspkind = lspkind
+
+local status_ok,cmp = pcall(require, 'cmp')
+if not status_ok then
+    return
+end
 
 cmp.setup({
 	snippet = {
