209747a63770e9461f58fbefd1c2ce3eb76a4365
philbev <phil@philbev.co.uk>
2022-09-21 09:37:06 +0100

    colorizer.lua: Protected call for colorizer.

--- lua/setup/colorizer.lua
+++ lua/setup/colorizer.lua
@@ -7,20 +7,25 @@
 
 -- ~/.config/nvim/lua/setup/colorizer.lua
 
---DEFAULT_OPTIONS = {
---	RGB      = true;         -- #RGB hex codes
---	RRGGBB   = true;         -- #RRGGBB hex codes
---	names    = true;         -- "Name" codes like Blue
---	RRGGBBAA = true;        -- #RRGGBBAA hex codes
---	rgb_fn   = false;        -- CSS rgb() and rgba() functions
---	hsl_fn   = false;        -- CSS hsl() and hsla() functions
---	css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
---	css_fn   = true;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
---	-- Available modes: foreground, background
---	mode     = 'background'; -- Set the display mode.
---  }
+DEFAULT_OPTIONS = {
+	RGB      = true;         -- #RGB hex codes
+	RRGGBB   = true;         -- #RRGGBB hex codes
+	names    = true;         -- "Name" codes like Blue
+	RRGGBBAA = true;        -- #RRGGBBAA hex codes
+	rgb_fn   = false;        -- CSS rgb() and rgba() functions
+	hsl_fn   = false;        -- CSS hsl() and hsla() functions
+	css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn   = true;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	-- Available modes: foreground, background
+	mode     = 'background'; -- Set the display mode.
+  }
 
-require 'colorizer'.setup()
+local status_ok,colorizer = pcall(require, 'colorizer')
+if not status_ok then
+    return
+end
+
+colorizer.setup()
 
 -- Attach to certain Filetypes, add special configuration for `html`
 -- Use `background` for everything else.
