7b908850c2be7d1342b6544a4e04a228f1ea84c2
philbev <phil@philbev.co.uk>
2022-09-30 10:42:03 +0100

    autocmds.lua: Variable name change.

--- lua/setup/autocmds.lua
+++ lua/setup/autocmds.lua
@@ -7,65 +7,65 @@
 
 -- ~/.config/nvim/lua/setup/autocmds.lua
 
-local cmd = vim.api.nvim_create_autocmd
+local au = vim.api.nvim_create_autocmd
 
 -- Highlight yanked text
-cmd({ 'TextYankPost' }, {
+au({ 'TextYankPost' }, {
     pattern = { '*' },
     command = "lua vim.highlight.on_yank({ timeout = 1000, higroup = IncSearch })",
 })
 
 -- Display cursorline on active windows only.
-cmd({ 'BufEnter', 'WinEnter' }, {
+au({ 'BufEnter', 'WinEnter' }, {
     pattern = '*',
     command = "lua vim.o.cursorline = true"
 })
-cmd({ 'WinLeave' }, {
+au({ 'WinLeave' }, {
     pattern = '*',
     command = "lua vim.o.cursorline = false"
 })
 
 -- No comments to be generated after 'o' or after hitting 'Enter'.
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = '*',
     command = "lua vim.opt.formatoptions:remove { 'r', 'o' }"
 })
 
 -- Use <Space> and 'b' keys to page forward and back in read-only buffers.
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = { 'packer', 'checkhealth', 'help' },
     command = "lua vim.keymap.set('n', '<Space>', '<C-f>', { noremap = true, silent = true, buffer = 0 })"
 })
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = { 'packer', 'checkhealth', 'help' },
     command = "lua vim.keymap.set('n', 'b', '<C-b>', { noremap = true, silent = true, buffer = 0 })"
 })
 
 -- Hit <Esc> key to quit packer.
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = { 'packer' },
     command = "lua vim.keymap.set('n', '<Esc>', '<cmd>q<cr>', { noremap = true, silent = true, buffer = 0 })"
 })
 
 -- Use 'Enter' key to follow hyperlinks.
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = { 'help' },
     command = "lua vim.keymap.set('n', '<cr>', '<C-]>', { noremap = true, silent = true, buffer = 0 })"
 })
 
 -- 'Insert' key moves cursor to next hyperlink.
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = { 'help' },
     command = "lua vim.keymap.set('n', '<Insert>', '/|[^|]*|<cr>:nohlsearch<cr>', { noremap = true, silent = true, buffer = 0 })"
 })
 
 -- 'Del' key moves cursor to previous hyperlink.
-cmd({ 'FileType' }, {
+au({ 'FileType' }, {
     pattern = { 'help' },
     command = "lua vim.keymap.set('n', '<Del>', '?|[^|]*|<cr>:nohlsearch<cr>', { noremap = true, silent = true, buffer = 0 })"
 })
 
-cmd({'FileType'}, {
+au({'FileType'}, {
     pattern = {'lua'},
     command = "lua vim.keymap.set('n', '<F5>', '<cmd>w | terminal lua %<cr>a', { noremap = true, silent = true, buffer = 0 })"
 })
@@ -75,7 +75,7 @@ cmd({'FileType'}, {
 --    command = "0r ~/.config/nvim/template/skeleton.lua"
 --})
 
-cmd({'FileType'}, {
+au({'FileType'}, {
     pattern = {'python'},
     command = "lua vim.keymap.set('n', '<F5>', '<cmd>w | terminal python %<cr>a', { noremap = true, silent = true, buffer = 0 })"
 })
