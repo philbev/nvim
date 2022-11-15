f86e78832ce6bfcdda196e220b72c9df93fe99ec
philbev <phil@philbev.co.uk>
2022-11-15 10:11:32 +0000

    mason.lua: Addition of figlet banner.

--- lua/setup/mason.lua
+++ lua/setup/mason.lua
@@ -1,3 +1,12 @@
+--                                    _             
+--  _ __ ___   __ _ ___  ___  _ __   | |_   _  __ _ 
+-- | '_ ` _ \ / _` / __|/ _ \| '_ \  | | | | |/ _` |
+-- | | | | | | (_| \__ \ (_) | | | |_| | |_| | (_| |
+-- |_| |_| |_|\__,_|___/\___/|_| |_(_)_|\__,_|\__,_|
+--                                                  
+
+-- ~/.config/nvim/lua/setup/mason.lua
+
 local status_ok, mason = pcall(require, 'mason')
 if not status_ok then
     return
